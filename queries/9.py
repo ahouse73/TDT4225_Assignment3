add9b9ffbd233fd169f537f58624be18786709a8
Arne Putzeys <arneputzeys@student.kuleuven.be>
2023-10-26 16:49:43 +0200

    Updated query 9 to be more efficient

--- queries/9.py
+++ queries/9.py
@@ -4,6 +4,7 @@ from decouple import config
 from datetime import datetime
 
 # Find all users who have invalid activities, and the number of invalid activities per user
+# https://stackoverflow.com/questions/32264225/how-to-get-multiple-document-using-array-of-mongodb-id
 class Query:
 
     def __init__(self):
@@ -13,37 +14,36 @@ class Query:
         
     def perform_query(self):
         
-        # 1. Per Activity, get all trackpoints
+        # 1. Per activity, get all the trackpoints
+        # 2. Check if they qualify to be invalid
+        # 3. If invalid, add to dict
+        
         activities = self.db['Activity'].find()
-        invalid_users_dict = {}
+        
+        invalid_users_dict = {} # Bookkeeping
         for act in activities:
             user_id = act['user_id']
-            trackpoints = act['trackpoints']
-            amount_of_trackpoints = len(trackpoints)
-        
+            trackpoints = list(self.db['TrackPoint'].find({"_id":{"$in":act['trackpoints']}}, {'Date':1, '_id':1})) # This returns all sequential trackpoints
+            
+            
             date_format = '%Y-%m-%dT%H:%M:%S.%f'
-            for index, trackpoint_id in enumerate(trackpoints):
-                # To prevent index out of bounds error
-                if index == 0:
-                    current_trackpoint = self.db['TrackPoint'].find_one({"_id":trackpoint_id})
-                    current_trackpoint_timestamp = datetime.strptime(current_trackpoint['Date'], date_format)
-                    continue
-                if index == amount_of_trackpoints:
+            for index, tp in enumerate(trackpoints):
+                
+                if index == 0: # First trackpoint does not have anything to compare to
+                    prev_timestamp = datetime.strptime(tp['Date'], date_format)
                     continue
                 
-                next_trackpoint = self.db['TrackPoint'].find_one({"_id":trackpoint_id})
-                next_trackpoint_timestamp = datetime.strptime(next_trackpoint['Date'], date_format)
+                current_timestamp = datetime.strptime(tp['Date'], date_format)
                 
-                if (next_trackpoint_timestamp - current_trackpoint_timestamp).total_seconds() / 60 > 5:
+                if (current_timestamp - prev_timestamp).total_seconds() > 300:
                     if user_id in invalid_users_dict:
                         invalid_users_dict[user_id] += 1
                     else:
                         invalid_users_dict[user_id] = 1
-                        pprint(user_id)
-                    
-                current_trackpoint = next_trackpoint
-                current_trackpoint_timestamp = datetime.strptime(next_trackpoint['Date'], date_format)
+                        #pprint(user_id)
                 
+                prev_timestamp = current_timestamp
+  
         pprint(invalid_users_dict)
         self.close()    
     
